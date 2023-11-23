b2946d43dbdb6a90c57c8fd5b12c0e0ffa3a5a02
Roy Atali <58545785+roy845@users.noreply.github.com>
2023-11-23 18:17:56 +0200

    Delete backend directory

--- backend/app/database.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from .config import settings
-
-
-client = MongoClient(settings.DATABASE_URI)
-print('🚀 Connected to MongoDB...')
-db = client.TodoApp  # database name
-Todo = db["todos"]  # collection name
-User = db["users"]  # collection name
